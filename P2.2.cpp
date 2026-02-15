fe6a4877e449ba3ac6e22d122f5487a579f5ebb1
Vansh28v <vanshjani7116@gmail.com>
2026-02-15 22:12:23 +0530

    Add files via upload

---
+++ P2.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Library
+{
+private:
+
+    int Book_id;
+    string Book_name;
+    string Author_name;
+    int copies;
+
+};
